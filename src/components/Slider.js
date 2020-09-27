02bb74bdf038aacecdb4bccef5c780bd85d96fb4
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-09-27 19:59:25 -0400

    arrow onclick edgecases & impliementation

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -11,18 +11,45 @@ const Slider = props => {
   const getWidth = () => window.innerWidth
 
   const [state, setState] = useState({
+    activeIndex: 0,
     translate: 0,
     transition: 0.45
   })
 
-  const { translate, transition } = state
+  const { translate, transition, activeIndex } = state
 
   const prevSlide = () => {
       console.log('prevSlide');
+      if (activeIndex === 0) {
+        return setState({
+          ...state,
+          translate: (props.slides.length - 1) * getWidth(),
+          activeIndex: props.slides.length - 1
+        })
+      }
+  
+      setState({
+        ...state,
+        activeIndex: activeIndex - 1,
+        translate: (activeIndex - 1) * getWidth()
+      })
   }  
 
   const nextSlide = () => {
     console.log('nextSlide');
+    if (activeIndex === props.slides.length - 1) {
+      return setState({
+        ...state,
+        translate: 0,
+        activeIndex: 0
+      })
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex + 1,
+      translate: (activeIndex + 1) * getWidth()
+    })
   }   
 
   return (
