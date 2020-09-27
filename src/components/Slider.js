4b47f424aee88d1ff491b33d084e40971c3e8ae3
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-09-27 19:52:27 -0400

    adding Arrows component & button click

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { css, jsx } from '@emotion/core'
 import SliderContent from './SliderContent'
 import Slide from './Slide'
-
+import Arrow from './Arrow'
 /**
  * @function Slider
  */
@@ -17,17 +17,30 @@ const Slider = props => {
 
   const { translate, transition } = state
 
+  const prevSlide = () => {
+      console.log('prevSlide');
+  }  
+
+  const nextSlide = () => {
+    console.log('nextSlide');
+  }   
+
   return (
     <div css={SliderCSS}>
       <SliderContent
         translate={translate}
         transition={transition}
-        width={getWidth()}
+        width={getWidth() * props.slides.length}
       >
         {props.slides.map((slide, i) => (
           <Slide key={slide + i} content={slide} />
         ))}
       </SliderContent>
+
+
+      <Arrow direction="left" handleClick={prevSlide} />
+      <Arrow direction="right" handleClick={nextSlide} />
+
     </div>
   )
 }
