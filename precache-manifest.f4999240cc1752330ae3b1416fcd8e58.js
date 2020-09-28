d0ae49f5a5535c16e6e9cb5fb883c1d5331bb002
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-09-27 22:35:50 -0400

    Updates

---
+++ precache-manifest.f4999240cc1752330ae3b1416fcd8e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92bb7208275ed22cc86d2acef9e5db0",
+    "url": "/react-image-carousel/index.html"
+  },
+  {
+    "revision": "3a8a776fb4c4e44b5bbc",
+    "url": "/react-image-carousel/static/js/2.2928dec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-carousel/static/js/2.2928dec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded14f0f8016c2a53d7a",
+    "url": "/react-image-carousel/static/js/main.29a9662f.chunk.js"
+  },
+  {
+    "revision": "bf99a4b3afe43b01a43a",
+    "url": "/react-image-carousel/static/js/runtime-main.fd0fe7b2.js"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/react-image-carousel/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/react-image-carousel/static/media/right-arrow.5e5b72cc.svg"
+  }
+]);
